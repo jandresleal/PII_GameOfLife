9f0370ccf1e39a31306f6f8b9c7742f785c7f579
matoferreira <matifer1@gmail.com>
2021-04-29 20:51:24 -0300

    Agregado BoardLoader

---
+++ src/Library/BoardLoader.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace PII_Game_Of_Life
+{
+    public class BoardLoader
+    {
+        public static string url = "assets\board.txt";
+
+        void CreadorMatriz()
+        {
+
+        }
+    }
+}
