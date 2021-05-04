3289ebe0813a6611ababfbcf9e83cd833068218f
jandresleal <62621149+jandresleal@users.noreply.github.com>
2021-05-04 17:18:14 -0300

    cambios a board

--- src/Library/Board.cs
+++ src/Library/Board.cs
@@ -2,17 +2,32 @@ using System;
 
 namespace PII_Game_Of_Life
 {
-    public class board
+    public class Board
     {
         bool[,] board;
-        int boardWidth {get; set;}
-        int boardHeigth {get; set;}
+        public int BoardWidth {get; set;}
+        public int BoardHeigth {get; set;}
         public Board(int width, int height, bool[,] board)
         {
-            this.boardWidth = width;
-            this.boardHeigth = height;
+            this.BoardWidth = width;
+            this.BoardHeigth = height;
             this.board = new bool[width, height];
+            for (int x = 0; x <= width; x++)
+                {
+                    for (int y = 0; y <= height; y++)
+                    {
+                        this.board[x,y] = board[x,y];
+                    }
+                }
 
+        }
+        public void SetCell(int x, int y, bool viva)
+        {
+            this.board[x,y] = viva;
+        }
+         public bool GetCell(int x, int y)
+        {
+            return this.board[x,y];
         }
     }
 }
